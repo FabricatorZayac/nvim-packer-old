c1247c3b509d2f5e2f71276039e724f86f2a22dc
Eugene Fedchuk <fed4chuck@gmail.com>
2023-03-31 17:34:46 +0300

    copy to clipboard

--- lua/zayac/remap.lua
+++ lua/zayac/remap.lua
@@ -15,5 +15,5 @@ vim.keymap.set("x", "<leader>p", "\"_dp")
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 -- yank into clipboard, don't seem to work via ssh
--- vim.keymap.set("n", "<leader>y", "\"+y")
--- vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
