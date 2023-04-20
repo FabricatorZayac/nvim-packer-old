2ecac3d962c39bf4090606bdbb85d1e2cc08f24d
Eugene Fedchuk <fed4chuck@gmail.com>
2023-04-21 02:36:58 +0300

    Added pull map

--- after/ftplugin/fugitive.lua
+++ after/ftplugin/fugitive.lua
@@ -1,1 +1,2 @@
-vim.keymap.set("n", "pp", function () vim.cmd("Git push") end, { buffer = 0 })
+vim.keymap.set("n", "pp", function () vim.cmd("Git push") end, { buffer = 0, noremap = true })
+vim.keymap.set("n", "Fp", function () vim.cmd("Git pull") end, { buffer = 0, noremap = true })
