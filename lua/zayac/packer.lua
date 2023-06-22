e3bdf20de2aee49aa2c3d576328a3f372b09f141
Eugene Fedchuk <fed4chuck@gmail.com>
2023-06-22 05:28:52 +0300

    added org mode

--- lua/zayac/packer.lua
+++ lua/zayac/packer.lua
@@ -1,10 +1,11 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function (use)
-  use 'wbthomason/packer.nvim'
+  use "wbthomason/packer.nvim"
 
+  -- telescope
   use {
-    'nvim-telescope/telescope.nvim',
+    "nvim-telescope/telescope.nvim",
     tag = '0.1.1',
     requires = {'nvim-lua/plenary.nvim'}
   }
@@ -14,39 +15,43 @@ return require('packer').startup(function (use)
   }
   use "nvim-telescope/telescope-project.nvim"
 
+  -- colorscheme
   use "ellisonleao/gruvbox.nvim"
 
+  -- modeline
   use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    "nvim-lualine/lualine.nvim",
+    requires = { "kyazdani42/nvim-web-devicons", opt = true }
   }
 
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'nvim-treesitter/playground'
-  use 'nvim-treesitter/nvim-treesitter-context'
+  -- treesitter
+  use "nvim-treesitter/nvim-treesitter"
+  use "nvim-treesitter/playground"
+  use "nvim-treesitter/nvim-treesitter-context"
 
-  use 'mbbill/undotree'
+  use "mbbill/undotree"
 
   use "cohama/lexima.vim"
 
+  -- lsp
   use {
-    'VonHeikemen/lsp-zero.nvim',
+    "VonHeikemen/lsp-zero.nvim",
     branch = 'v1.x',
     requires = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
-      {'williamboman/mason.nvim'},           -- Optional
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+      {"neovim/nvim-lspconfig"},             -- Required
+      {"williamboman/mason.nvim"},           -- Optional
+      {"williamboman/mason-lspconfig.nvim"}, -- Optional
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},         -- Required
-      {'hrsh7th/cmp-nvim-lsp'},     -- Required
-      {'hrsh7th/cmp-buffer'},       -- Optional
-      {'hrsh7th/cmp-path'},         -- Optional
-      {'saadparwaiz1/cmp_luasnip'}, -- Optional
-      {'hrsh7th/cmp-nvim-lua'},     -- Optional
+      {"hrsh7th/nvim-cmp"},         -- Required
+      {"hrsh7th/cmp-nvim-lsp"},     -- Required
+      {"hrsh7th/cmp-buffer"},       -- Optional
+      {"hrsh7th/cmp-path"},         -- Optional
+      {"saadparwaiz1/cmp_luasnip"}, -- Optional
+      {"hrsh7th/cmp-nvim-lua"},     -- Optional
       -- Snippets
-      {'L3MON4D3/LuaSnip'},             -- Required
-      {'rafamadriz/friendly-snippets'}, -- Optional
+      {"L3MON4D3/LuaSnip"},             -- Required
+      {"rafamadriz/friendly-snippets"}, -- Optional
     }
   }
 
@@ -84,24 +89,19 @@ return require('packer').startup(function (use)
     config = function () vim.g.instant_username = "FabricatorZayac" end
   }
 
-  -- use {
-  --   "startup-nvim/startup.nvim",
-  --   requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
-  --   config = function ()
-  --     require"startup".setup()
-  --   end
-  -- }
-
+  -- bullshit language stuff
+  use "nvim-orgmode/orgmode"
+  use "vmchale/ats-vim"
   use "jose-elias-alvarez/null-ls.nvim"
 
-  use 'nvim-tree/nvim-web-devicons'
-  use 'norcalli/nvim-colorizer.lua'
+  -- I don't even remember what this was for
+  use "nvim-tree/nvim-web-devicons"
+  use "norcalli/nvim-colorizer.lua"
 
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-surround'
-  use 'tpope/vim-repeat'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-obsession'
-
-  use 'vmchale/ats-vim'
+  -- mandatory tpope stuff
+  use "tpope/vim-commentary"
+  use "tpope/vim-surround"
+  use "tpope/vim-repeat"
+  use "tpope/vim-fugitive"
+  use "tpope/vim-obsession"
 end)
